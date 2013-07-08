2289317c1521d23e817e236c2ca63b0de7ff6366
Peter Odding <peter@peterodding.com>
2013-07-08 19:56:23 +0200

    Move version string to Python module (preparing for Sphinx)

--- setup.py
+++ setup.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 
+# Setup script for redock.
+#
+# Author: Peter Odding <peter@peterodding.com>
+# Last Change: July 8, 2013
+# URL: https://github.com/xolox/python-redock
+
+import re
 from os.path import abspath, dirname, join
 from setuptools import setup, find_packages
 
 # Find the directory containing the source distribution.
-directory = dirname(abspath(__file__))
+source_directory = dirname(abspath(__file__))
+
+# Find the current version.
+module = join(source_directory, 'redock', '__init__.py')
+for line in open(module, 'r'):
+    match = re.match(r'^__version__\s*=\s*["\']([^"\']+)["\']$', line)
+    if match:
+        version_string = match.group(1)
+        break
+else:
+    raise Exception, "Failed to extract version from redock/__init__.py!"
 
 # Fill in the long description (for the benefit of PyPi)
 # with the contents of README.rst (rendered by GitHub).
-readme_text = open(join(directory, 'README.rst'), 'r').read()
+readme_text = open(join(source_directory, 'README.rst'), 'r').read()
 
 # Fill in the "install_requires" field based on requirements.txt.
-requirements = [l.strip() for l in open(join(directory, 'requirements.txt'), 'r')]
+requirements = [l.strip() for l in open(join(source_directory, 'requirements.txt'), 'r')]
 
 setup(name='redock',
-      version='0.3',
+      version=version_string,
       description="Human friendly wrapper around Docker",
       long_description=readme_text,
       url='https://github.com/xolox/python-redock',
